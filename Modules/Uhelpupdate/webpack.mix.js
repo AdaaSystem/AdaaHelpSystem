ef1ca814e0f97fe7fdda11b022fd45f8490062f9
Shift <shift@laravelshift.com>
2022-08-16 15:42:33 +0000

    Use ES modules

--- Modules/Uhelpupdate/webpack.mix.js
+++ Modules/Uhelpupdate/webpack.mix.js
@@ -2,7 +2,7 @@ const dotenvExpand = require('dotenv-expand');
 dotenvExpand(require('dotenv').config({ path: '../../.env'/*, debug: true*/}));
 
 const mix = require('laravel-mix');
-require('laravel-mix-merge-manifest');
+import 'laravel-mix-merge-manifest';
 
 mix.setPublicPath('../../public').mergeManifest();
 
